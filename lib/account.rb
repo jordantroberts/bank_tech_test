4d3fe1fec404e700cf8f86ecdd9f651e925202ef
Jordan Roberts <jordanroberts@talk21.com>
2019-06-03 15:15:31 +0100

    New account opens with no credit/debit history, and todays date

--- lib/account.rb
+++ lib/account.rb
@@ -1,9 +1,12 @@
 class Account
 
   # responsible for handling the transactions: deposit, withdraw, balance.
-  attr_reader :balance
-  
+  attr_reader :balance, :credit, :debit, :date
+
   def initialize
     @balance = 0.00
+    @credit = 0.00
+    @debit = 0.00
+    @date = Date.today.strftime("%d/%m/%Y")
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -5,7 +5,19 @@ describe Account do
 
   describe '#initialize' do
     it 'starts with a balance of zero' do
-      expect(account.balance).to eq(0.00) 
+      expect(account.balance).to eq(0.00)
+    end
+
+    it 'starts with no credit history' do
+      expect(account.credit).to eq(0.00)
+    end
+
+    it 'starts with no debit history' do
+      expect(account.debit).to eq(0.00)
+    end
+
+    it "opens with today's date" do
+      expect(account.date).to eq(Date.today.strftime("%d/%m/%Y"))
     end
   end
 end
