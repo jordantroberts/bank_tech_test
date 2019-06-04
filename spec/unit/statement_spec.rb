5ef8485d3bf71adae38aae8b8ef528adb0150acc
Jordan Roberts <jordanroberts@talk21.com>
2019-06-04 16:45:12 +0100

    Fixed mocking ... I think

--- spec/unit/statement_spec.rb
+++ spec/unit/statement_spec.rb
@@ -3,19 +3,21 @@
 require 'statement'
 
 describe Statement do
-  let(:statement) { described_class.new }
 
   describe '#initialize' do
     it 'starts with an empty statement' do
-      expect(statement.display).to eq []
+      expect(subject.display).to eq []
     end
   end
 
   describe '#format' do
-    let(:account) { Account.new(statement = Statement.new) }
     it 'prints the statement as a table' do
+      account = double(Account.new(statement = Statement.new))
+      expect(account).to receive(:deposit)
       account.deposit(15.00)
+      expect(account).to receive(:complete_transaction)
       account.complete_transaction
+      expect(account).to receive_message_chain(:statement, :format)
       expect(account.statement.format).to eq Date.today.strftime('%d/%m/%Y') + ' || 15.00 ||  || 15.00'
     end
   end
